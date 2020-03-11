46983952f5f5c30786d7b069748b28a62ac2461a
Student <example@example.com>
2020-03-10 20:13:11 -0700

    add reset button

--- js/scripts.js
+++ js/scripts.js
@@ -4,32 +4,32 @@ var score = 0
 // user interface logic
 $(document).ready(function() {
   $("#openDoor").click(function() {
-    $("#frontDoor").hide();
-    $("#foyer").show();
+    $("#frontDoor").fadeOut();
+    $("#foyer").fadeIn();
     score += 1;
   });
   $(".runAway").click(function() {
-    $("#frontDoor").hide();
-    $("#coward").show();
+    $("#frontDoor").fadeOut();
+    $("#coward").fadeIn();
     alert("Your score is " + score);
   });
   $("#investigate").click(function() {
-    $("#foyer").hide();
-    $("#monster").show();
+    $("#foyer").fadeOut();
+    $("#monster").fadeIn();
     score += 1;
   });
   $("#fight").click(function() {
-    $("#monster").hide();
-    $("#death").show();
+    $("#monster").fadeOut();
+    $("#death").fadeIn();
     alert("Your score is " + score);
   });
   $("#befriend").click(function() {
-    $("#monster").hide();
-    $("#pizzaParty").show();
+    $("#monster").fadeOut();
+    $("#pizzaParty").fadeIn();
     score += 2;
     alert("Your score is " + score);
   });
   $(".tryAgain").click(function() {
     location.reload(true);
-  }
+  });
 });
