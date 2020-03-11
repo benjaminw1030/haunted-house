07915760641e41e86185083e0bea77a9fd69b072
Student <example@example.com>
2020-03-10 20:18:36 -0700

    setup fadeIn styling in JS

--- js/scripts.js
+++ js/scripts.js
@@ -4,28 +4,28 @@ var score = 0
 // user interface logic
 $(document).ready(function() {
   $("#openDoor").click(function() {
-    $("#frontDoor").fadeOut();
+    $("#frontDoor").hide();
     $("#foyer").fadeIn();
     score += 1;
   });
   $(".runAway").click(function() {
     $("#frontDoor").hide();
     $("#foyer").hide();
-    $("#coward").show();
+    $("#coward").fadeIn();
     alert("Your score is " + score);
   });
   $("#investigate").click(function() {
-    $("#foyer").fadeOut();
+    $("#foyer").hide();
     $("#monster").fadeIn();
     score += 1;
   });
   $("#fight").click(function() {
-    $("#monster").fadeOut();
+    $("#monster").hide();
     $("#death").fadeIn();
     alert("Your score is " + score);
   });
   $("#befriend").click(function() {
-    $("#monster").fadeOut();
+    $("#monster").hide();
     $("#pizzaParty").fadeIn();
     score += 2;
     alert("Your score is " + score);
